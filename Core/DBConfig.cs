c5be0fbae8da5e59286739a67d8e3c4dc68d1634
NoeSagan <noeliapichardo0624@gmail.com>
2026-05-27 20:55:15 -0400

    Quitar Configuración de la base de .gitignore

---
+++ Core/DBConfig.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+
+namespace ISW_II_2
+{
+    public static class DBConfig
+    {
+        public static string ConnectionString => GetConnectionString();
+
+        public static string GetConnectionString()
+        {
+            string host = "db.enwuowrovwgtcfupsmqr.supabase.co";
+            string port = "5432";
+            string user = "postgres";
+            string pass = "NoeliaDBISWII";
+            string db = "postgres";
+            return $"Host={host};Port={port};Username={user};Password={pass};Database={db}";
+        }
+    }
+}
